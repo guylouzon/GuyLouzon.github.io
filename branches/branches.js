7a108388b45ec4a54f7403c157cc3f099697c073
Guy L <guylou@outlook.com>
2019-05-10 15:05:53 +0300

    Update branches.js

--- branches/branches.js
+++ branches/branches.js
@@ -1,57 +1,60 @@
 class branches {
+	/*
+	(c) Guy Louzon, guylou@outlook.com, GNU 3
+	*/
+	constructor() {
+		this.initBranches();
+		this.leaves = {};
+		this.initLeaves();
+	}
 
 
-constructor() {
-	this.initBranches();
-}
 
+	initBranches() {
+	 var branch = {};
+	 var tempstr = "";
+	 var comment = "";
+	 var nods = document.getElementsByClassName("branches");
+		for (var i = 0;i < nods.length;i++) {
 
-
-initBranches() {
- var branch = {};
- var tempstr = "";
- var comment = "";
- var nods = document.getElementsByClassName("branches");
-	for (var i = 0;i < nods.length;i++) {
-
-	  tempstr = nods[i].innerHTML;
-	  var varNames = tempstr.match(/\{\$(.*?)\}/i); // match groups! use exec
-	  if (varNames !== null) {
-		nods[i].classList.add("branch_html_" + varNames[1]);
-		comment = document.createComment(tempstr);
-		nods[i].insertBefore(comment, nods[i].firstChild);
-	  }
-	 
-			if (nods[i].hasAttributes()) {
-				var attribs = nods[i].attributes;
-		  var tempstr2 = '';
-		  var varNames2 = [];
-		  var attr = [];
-		  var newattrname = '';
-		  var newattr2add = [];
-				for (var j = 0;j < attribs.length;j++) {
-					tempstr2 = attribs[j].value;
-			varNames2 = tempstr2.match(/\{\$(.*?)\}/i);
-			if (varNames2 !== null)  {
-			
-			newattrname = attribs[j].name;
-			attr = [attribs[j].name, varNames2[1], tempstr2]; //name,varname,org
-			newattr2add.push(attr);        
-			}
-				}
-		  
-		  for (j = 0;j < newattr2add.length;j++) {
-			attr = newattr2add.pop();
-			nods[i].setAttribute("branch_attrib_key_" + attr[1] , attr[2]);
-			nods[i].setAttribute("branch_attrib_key_name" , attr[0]);
-			nods[i].classList.add("branch_attrib_" + attr[1]);
-
+		  tempstr = nods[i].innerHTML;
+		  var varNames = tempstr.match(/\{\$(.*?)\}/i); // match groups! use exec
+		  if (varNames !== null) {
+			nods[i].classList.add("branch_html_" + varNames[1]);
+			comment = document.createComment(tempstr);
+			nods[i].insertBefore(comment, nods[i].firstChild);
 		  }
+		 
+				if (nods[i].hasAttributes()) {
+					var attribs = nods[i].attributes;
+			  var tempstr2 = '';
+			  var varNames2 = [];
+			  var attr = [];
+			  var newattrname = '';
+			  var newattr2add = [];
+					for (var j = 0;j < attribs.length;j++) {
+						tempstr2 = attribs[j].value;
+				varNames2 = tempstr2.match(/\{\$(.*?)\}/i);
+				if (varNames2 !== null)  {
+				
+				newattrname = attribs[j].name;
+				attr = [attribs[j].name, varNames2[1], tempstr2]; //name,varname,org
+				newattr2add.push(attr);        
+				}
+					}
+			  
+			  for (j = 0;j < newattr2add.length;j++) {
+				attr = newattr2add.pop();
+				nods[i].setAttribute("branch_attrib_key_" + attr[1] , attr[2]);
+				nods[i].setAttribute("branch_attrib_key_name" , attr[0]);
+				nods[i].classList.add("branch_attrib_" + attr[1]);
+
+			  }
+			  
+			}
 		  
-		}
-	  
+		  }
 	  }
-  }
   
   plantHTML(key,value) {
    var nods = document.getElementsByClassName("branch_html_" + key);
@@ -88,14 +91,62 @@ initBranches() {
   }
 
   
-  climbABranch(bran) {
-   	for (var i in bran) {
-		this.plantHTML(i,bran[i]);
-		this.plantAttrib(i,bran[i]);
+	climbABranch(bran) {
+		for (var i in bran) {
+			this.plantHTML(i,bran[i]);
+			this.plantAttrib(i,bran[i]);
+		}
+	  
+	}
+	
+	initLeaves() {
+		var nods = document.getElementsByClassName("leaves");
+		for (var i = 0;i < nods.length;i++) {
+			var leafId = nods[i].getAttribute("id");
+			if (leafId == undefined) break;
+			var tempstr = nods[i].innerHTML;
+			var leaf = {};
+			leaf['leafId'] = leafId;
+			leaf['tmplt'] = tempstr;
+			leaf['vars'] = tempstr.match(/\{\$(.*?)\}/ig);
+			this.leaves[leafId] = leaf;
+		}
+		console.log("this.leaves ");
+		console.log(this.leaves);
+	}
+	
+	sprout(leafId,params) {
+		console.log(params);
+		var tempstr = this.leaves[leafId].tmplt;
+		var tmp2 = '';
+		var vars = this.leaves[leafId].vars;
+		console.log("vars");
+		console.log(vars);
+		for (var i in params) {
+			tempstr = this.replaceAll(tempstr,'{\\$' + i + '}',params[i]);
+
+		}
+		
+		return tempstr;
+	}
+
+    replaceAll(str, find, replace) {
+        return str.replace(new RegExp(find, 'g'), replace);
     }
   
-  
-  }
-  
-  
+	leavesLuv(leafId,itemsjson,append = 1) {
+
+		var Luv = '';
+		for (var key in itemsjson) {
+			Luv += this.sprout(leafId,itemsjson[key]);
+		}
+		var elem = document.getElementById(leafId);
+		
+		tempHTML = '';
+		if (append == 1) {
+			var tempHTML = elem.innerHTML;
+		}
+		elem.innerHTML = tempHTML + Luv;
+		
+	}
 }
