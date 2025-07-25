3cb6aa403e44e930f6293e2157479b7fce6a3918
Guy Louzon <guylou@outlook.com>
2025-07-25 08:59:27 +0300

    Update manifest.js, link to icons

--- timelink/manifest.js
+++ timelink/manifest.js
@@ -8,12 +8,12 @@
   "description": "Timelink PWA",
   "icons": [
     {
-      "src": "icon-192.png",
+      "src": "icons/tl192.png",
       "sizes": "192x192",
       "type": "image/png"
     },
     {
-      "src": "icon-512.png",
+      "src": "icons/tl512.png",
       "sizes": "512x512",
       "type": "image/png"
     }
